33eaab0e17cc70ecb94f00886698a6d76096f88d
gyovannyvpn123 <mdanut159@gmail.com>
2025-07-04 20:55:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import WebSocket from 'ws';
-
-const WS_URL = 'wss://edge-messenger.facebook.com/chat'; // exemplu URL websocket
+import { buildLsReqMessage } from './utils.js';
+import { WS_URL, DEFAULT_VERSION_ID } from './constants.js';
 
 export class MessengerClient {
   constructor({ cookie }) {
@@ -8,8 +8,8 @@ export class MessengerClient {
     this.cookie = cookie;
     this.ws = null;
     this.requestId = 1;
-    this.epochId = null;
-    this.versionId = null;
+    this.epochId = Date.now();
+    this.versionId = DEFAULT_VERSION_ID;
   }
 
   connect() {
@@ -38,50 +38,34 @@ export class MessengerClient {
     });
   }
 
-  sendMessage(message) {
+  sendMessage(buffer) {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      this.ws.send(message);
-      console.log('Trimis:', message);
+      this.ws.send(buffer);
+      console.log('Trimis mesaj binar:', buffer);
     } else {
       console.error('WebSocket nu este conectat');
     }
-  }
-
-  handleMessage(data) {
-    // Aici parsezi mesajele primite
-    console.log('Mesaj primit:', data.toString());
   }
 
   nextRequestId() {
     return this.requestId++;
   }
 
-  // Exemplu trimite un ls_req simplu
   sendLsReq(tasks = []) {
-    if (!this.epochId) this.epochId = Date.now(); // poți inițializa altfel
-    if (!this.versionId) this.versionId = '24103687892584821'; // default
-
     const payload = {
       epoch_id: this.epochId,
-      tasks: tasks,
+      tasks,
       version_id: this.versionId,
-    };
-
-    const message = JSON.stringify({
-      app_id: "772021112871879",
-      payload: JSON.stringify(payload),
       request_id: this.nextRequestId(),
       type: 3
-    });
+    };
 
-    // Construiți mesajul cu prefixul necesar (ex: "2�\u0002\u0000\u0007/ls_req\u0000" + message)
-    const fullMessage = this.buildWebSocketMessage('/ls_req', message);
-    this.sendMessage(fullMessage);
+    const messageBuffer = buildLsReqMessage('/ls_req', payload);
+    this.sendMessage(messageBuffer);
   }
 
-  buildWebSocketMessage(path, jsonPayload) {
-    // Simplificat: doar trimite path + jsonPayload concatenat
-    // TODO: implementează serializarea binară conform protocolului real
-    return `2�\u0002\u0000\u0007${path}\u0000${jsonPayload}`;
+  handleMessage(data) {
+    // Deocamdată doar log
+    console.log('Mesaj primit:', data.toString());
   }
 }
